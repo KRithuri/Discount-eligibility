3f5ad5f84b21a0b6225a268754749ac06121f0eb
Karabo Rithuri <rithurikarabo@gmail.com>
2023-09-18 01:56:59 +0200

    getting membership status from user input

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function checkEligibility(){
-    var age = document.getElementById("age").value;
+    //getting age from user input
+    let age = document.getElementById("age").value;
+    //getting membership status from html/user input
+    let status = document.getElementById("status");
+    
+    if(Number(age)>=18){
+        document.getElementById("output").innerHTML="Eligible for discount";
+    }else{
+        document.getElementById("output").innerHTML="Not eligible for discount";
+    }
 }
