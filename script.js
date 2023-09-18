57d2b983f119419e12102c3122e40ffc9be84608
Karabo Rithuri <rithurikarabo@gmail.com>
2023-09-18 03:10:24 +0200

    checking conditions and displaying results

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ function checkEligibility(){
     //getting membership status from html/user input
     let status = document.getElementById("status");
 
-    if(Number(age) >= 65){
-        document.getElementById("output").innerHTML="Congratulations! You are Eligible for discount!";
-    }else{
-        document.getElementById("output").innerHTML="Not eligible for discount";
+    if (Number(age) >= 65) {
+        document.getElementById("output").innerHTML= "Congratulations! You qualify for the Senior discount!";
+        document.getElementById("output").style.color = "green";
+    } else if(Number(age) >= 18 && status.checked){
+        document.getElementById("output").innerHTML= "Congratulations! You are Eligible for discount!";
+        document.getElementById("output").style.color = "green";
+    } else { 
+        document.getElementById("output").innerHTML = "Unfortunately, you are not eligible for a discount!";
+        
+        document.getElementById("output").style.color = "red";
     }
 }
